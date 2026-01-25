ba8f86c76bc34f59e32d5f1e6bab1f16027acca3
mohamedshabaan223 <muhmddshabaan3@gmail.com>
2026-01-25 14:03:14 +0200

    make structure for app

---
+++ lib/fitness_app/fitness_app.dart
@@ -0,0 +1,14 @@
+import 'package:fitness_app/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+class FitnessApp extends StatelessWidget {
+  const FitnessApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
