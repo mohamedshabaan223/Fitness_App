858748ad4ee0df06716468e2816bfabbeda7aa2b
mohamedshabaan223 <muhmddshabaan3@gmail.com>
2026-01-30 04:23:28 +0200

    finish details screen

---
+++ lib/screens/details_screen.dart
@@ -0,0 +1,131 @@
+import 'package:fitness_app/common/colors_services.dart';
+
+import 'package:fitness_app/widgets/custom_bottom_navigation_bar.dart';
+import 'package:fitness_app/widgets/step_card.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class DetailsScreen extends StatefulWidget {
+  const DetailsScreen({super.key});
+
+  @override
+  State<DetailsScreen> createState() => _DetailsScreenState();
+}
+
+class _DetailsScreenState extends State<DetailsScreen> {
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    height: 300,
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(
+                          'https://th.bing.com/th/id/OIP.0Zj45EijfJY4a0V3xuYsrAHaFj?w=210&h=180&c=7&r=0&o=7&pid=1.7&rm=3',
+                        ),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Container(height: 300, color: Colors.transparent),
+                  Positioned(
+                    top: 50,
+                    left: 20,
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.arrow_back,
+                        color: Colors.white,
+                        size: 50,
+                      ),
+                      onPressed: () => Navigator.pop(context),
+                    ),
+                  ),
+                  Positioned.fill(
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Icon(
+                        Icons.play_circle_outline,
+                        color: Colors.white.withOpacity(0.8),
+                        size: 80,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Week Mass - Building Trainer",
+                      style: GoogleFonts.faustina(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w500,
+                        color: ColorsServices.black,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+                      style: GoogleFonts.openSans(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w400,
+                        color: ColorsServices.grey,
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    Text(
+                      "Workout Schedule",
+                      style: GoogleFonts.faustina(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w500,
+                        color: ColorsServices.black,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+
+                    Row(
+                      children: [
+                        Expanded(
+                          child: StepCard(
+                            step: "STEP 1",
+                            title: "Warm Up",
+                            time: "15 Mins",
+                            color: ColorsServices.red,
+                          ),
+                        ),
+                        SizedBox(width: 15),
+                        Expanded(
+                          child: StepCard(
+                            step: "STEP 2",
+                            title: "Basics Push",
+                            time: "20 Mins",
+                            color: ColorsServices.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+
+      bottomNavigationBar: CustomBottomNavigationBar(),
+    );
+  }
+}
--- lib/widgets/plan_card.dart
+++ lib/widgets/plan_card.dart
@@ -1,4 +1,5 @@
 import 'package:fitness_app/common/images_services.dart';
+import 'package:fitness_app/screens/details_screen.dart';
 import 'package:flutter/material.dart';
 
 class PlanCard extends StatelessWidget {
@@ -8,28 +9,33 @@ class PlanCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        color: Colors.grey.shade100,
-        borderRadius: BorderRadius.circular(10),
-        border: Border.all(color: Colors.grey.shade300),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Image.asset(ImagesServices.weighet),
-          const SizedBox(height: 10),
-          Text(
-            title,
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-          ),
-          const SizedBox(height: 5),
-          const Text(
-            'Walking is simple, yet powerful exercise for your body',
-            style: TextStyle(fontSize: 15, color: Colors.grey),
-          ),
-        ],
+    return InkWell(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (_)=>DetailsScreen()));
+      },
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          color: Colors.grey.shade100,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: Colors.grey.shade300),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset(ImagesServices.weighet),
+            const SizedBox(height: 10),
+            Text(
+              title,
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+            ),
+            const SizedBox(height: 5),
+            const Text(
+              'Walking is simple, yet powerful exercise for your body',
+              style: TextStyle(fontSize: 15, color: Colors.grey),
+            ),
+          ],
+        ),
       ),
     );
   }
